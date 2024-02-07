50bb3f7bbc1148fcc6fcadead2ee1a1eb6830100
simbld <s_bullado51@hotmail.com>
2024-02-07 23:57:47 +0100

    Initialize project structure and configurations for MySQL

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { databaseConfig } from "./config/database.config";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => databaseConfig
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
