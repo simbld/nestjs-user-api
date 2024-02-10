69c078f196c5251b19fca3d6b2b42ae6255b935e
simbld <s_bullado51@hotmail.com>
2024-02-10 23:59:20 +0100

    Test to try to make the Nest migration to my mysql database work

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,13 +1,14 @@
-import { DataSourceOptions } from "typeorm";
-import "dotenv/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const databaseConfig: DataSourceOptions = {
+export const databaseConfig: TypeOrmModuleOptions = {
   type: "mysql",
   host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT || "3306"),
+  port: parseInt(process.env.DATABASE_PORT, 10),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DB,
   entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+  migrations: [__dirname + "/../migrations/*{.ts,.js}"],
+  // Assurez-vous que synchronize est défini sur false en production !
   synchronize: false
 };
