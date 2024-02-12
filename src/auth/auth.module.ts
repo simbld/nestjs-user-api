5ead30fac10a2748651a4eea62aaebb28930d125
simbld <simon-bullado_student2022@wilder.school>
2024-02-13 00:50:08 +0100

    Update the Users Service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
 @Module({
   providers: [AuthService]
