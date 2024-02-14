8cc4232fb4080439794b7d761add67d46b4f40a0
simbld <simon-bullado_student2022@wilder.school>
2024-02-14 23:52:58 +0100

    Generate a controller for authentication and add endpoints for login and registration:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { User } from "../users/user.entity/user.entity";
 
-@Controller('auth')
-export class AuthController {}
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("login")
+  async login(@Body() user: User): Promise<any> {
+    return this.authService.login(user);
+  }
+
+  @Post("register")
+  async register(@Body() user: User): Promise<any> {
+    return this.authService.register(user);
+  }
+}
