8eae48552316670d8cb6eab3c49d4b18baa12f7a
simbld <simon-bullado_student2022@wilder.school>
2024-02-15 06:09:49 +0100

    Import the secret key that will be used to encrypt the access token into AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UsersModule } from "src/users/users.module";
-import { AuthController } from './auth.controller';
+import { AuthController } from "./auth.controller";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "./constants";
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtModule.register({ secret: jwtConstants.secret })],
   providers: [AuthService],
   controllers: [AuthController]
 })
