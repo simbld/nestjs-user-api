70a9d03b8186654b36d7b8ae7ad66e7b06a76ac8
simbld <simon-bullado_student2022@wilder.school>
2024-02-16 23:48:53 +0100

    Add strategy in the providers of your AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { UsersModule } from "src/users/users.module";
 import { AuthController } from "./auth.controller";
 import { JwtModule } from "@nestjs/jwt";
 import { jwtConstants } from "./constants";
+import { UsersService } from "src/users/users.service";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [UsersModule, JwtModule.register({ secret: jwtConstants.secret })],
-  providers: [AuthService],
+  providers: [AuthService, UsersService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
