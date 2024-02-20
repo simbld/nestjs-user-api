3613bd89300f20d891ff8b98c93fc7cfd890b809
simbld <simon-bullado_student2022@wilder.school>
2024-02-20 13:05:40 +0100

    feature: Add User and Article entities, relationships, fields, and status enum

--- src/articles/article.entity.ts
+++ src/articles/article.entity.ts
@@ -1,13 +1,27 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/users/user.entity/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Article {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  publishedAt: Date;
+
   @Column()
   title: string;
 
   @Column()
   content: string;
+
+  @Column({
+    type: "enum",
+    enum: ["draft", "published"],
+    default: "draft"
+  })
+  status: "draft" | "published";
+
+  @ManyToOne(() => User, (user) => user.articles)
+  author: User;
 }
