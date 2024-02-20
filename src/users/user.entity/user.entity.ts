e4ec82bab976389bde747a35afb0ac8c1c110e8e
simbld <simon-bullado_student2022@wilder.school>
2024-02-20 13:00:14 +0100

    features: Update User entity with status field and  createdAt field and relationships to Article

--- src/users/user.entity/user.entity.ts
+++ src/users/user.entity/user.entity.ts
@@ -1,9 +1,13 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Article } from "src/articles/article.entity";
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  createdAt: Date;
 
   @Column({ length: 50 })
   email: string;
@@ -16,4 +20,14 @@ export class User {
 
   @Column({ length: 80 })
   lastname: string;
+
+  @Column({
+    type: "enum",
+    enum: ["offline", "online", "busy", "away"],
+    default: "offline"
+  })
+  status: "offline" | "online" | "busy" | "away";
+
+  @OneToMany(() => Article, (article) => article.author)
+  articles: Article[];
 }
